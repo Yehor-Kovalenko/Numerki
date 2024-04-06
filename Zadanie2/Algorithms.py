0cda708b8ada879cf1e26ead5ec37264a6a59030
248678 Pivavarau Andrei <248678@edu.p.lodz.pl>
2024-04-07 01:26:23 +0200

    zbieznosc

--- Zadanie2/Algorithms.py
+++ Zadanie2/Algorithms.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 def solve(A, b, n):
-    M = findM(findN(findD(A)), findL(A), findU(A))
+    if niezbieznosc(A) | redukowalnosc(A):
+        return [0]
+    # M = findM(findN(A), findL(A), findU(A))
+    M = findM(A, findN(A))
     coef = findCoef(findN(findD(A)), b)
     res0 = zero(b, 1)
     res = zero(b, 1)
@@ -9,11 +12,46 @@ def solve(A, b, n):
         for i in range(len(coef)):
             res[i] += coef[i]
             for j in range(len(M)):
-                res[i] += (M[i][j] * res0[j])
+                res[i] += M[i][j] * res0[j]
         for i in range(len(res)):
             res0[i] = res[i]
         res = zero(b, 1)
     return res0
+
+def niezbieznosc(A):
+    res = 0
+    for i in range(len(A)):
+        sum = 0
+        for j in range(len(A)):
+            sum += A[i][j]
+        if sum - A[i][i] >= A[i][i]:
+            res += 1
+    if res == 0:
+        return False
+    else:
+        return True
+
+def redukowalnosc(A):
+    boolean = True
+    tmp_row = np.zeros(len(A))
+    tmp_col = np.zeros(len(A))
+    for i in range(len(A)):
+        for j in range(len(A)):
+            if A[i][j] != 0:
+                tmp_row[j] += 1
+            if A[j][i] != 0:
+                tmp_col[j] += 1
+    for k in range(len(A)):
+        if tmp_row[k] != 1:
+            boolean = False
+    if boolean:
+        return boolean
+    boolean = True
+    for k in range(len(A)):
+        if tmp_col[k] != 1:
+            boolean = False
+    return boolean
+
 
 def zero(matrix, d):
     zero = []
@@ -51,23 +89,32 @@ def findD(matrix):
     return res
 
 def findN(matrix):
-    res = matrix
+    res = []
     for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            if j == i:
-                res[i][j] = 1 / matrix[i][j]
+        if matrix[i][i] == 0:
+            res.append(0)
+        else:
+            res.append(1 / matrix[i][i])
     return res
 
-def findM(N, L, U):
-    for i in range(len(L)):
-        for j in range(len(L)):
-            L[i][j] += U[i][j]
-            L[i][j] = L[i][j] * N[i][i] * (-1)
-            if L[i][j] == -0.0:
-                L[i][j] = 0
-    return L
+# def findM(N, L, U):
+def findM(A, N):
+    M = zero(A, 2)
+    for i in range(len(A)):
+        for j in range(len(A)):
+            # L[i][j] += U[i][j]
+            # L[i][j] = L[i][j] * N[i] * (-1)
+            # if L[i][j] == -0.0:
+            #     L[i][j] = 0
+            # if i == j:
+            #     L[i][j] = 0
+            if i == j:
+                M[i][j] = 0
+            else:
+                M[i][j] = - (A[i][j] * N[i])
+    return M
 
 def findCoef(N, b):
     for i in range(len(b)):
-        b[i] *= N[i][i]
+        b[i] *= N[i]
     return b
