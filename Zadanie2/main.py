1d5acce16b83383ab58e607610246143050aeaea
248678 Pivavarau Andrei <248678@edu.p.lodz.pl>
2024-04-07 11:39:24 +0200

    read from file + dataset added

--- Zadanie2/main.py
+++ Zadanie2/main.py
@@ -1,21 +1,31 @@
-import math
-
-from numpy import double
-
-A = [[0.5, -0.0625, 0.1875, 0.0625], [-0.0625, 0.5, 0, 0], [0.1875, 0, 0.375, 0.125], [0.0625, 0, 0.125, 0.25]]
-# A = [[3, 3, 1], [2, 5, 7], [1, 2, 1]]
-# A = [[10, -1, 2], [-1, 11, -1], [2, -1, 10]]
-# A = [[0, 0, 1], [1, 0, 0], [0, 1, 0]]
-
-b = [1.5, -1.625, 1, 0.4375]
-# b = [12, 33, 8]
-# b = [3, 7, 5]
-
+import csv
+import numpy as np
 from Algorithms import solve
 
-res = solve(A, b, None, 0.1)
-if res == [0]:
-        print("Macierz nie spelnia warunkow zbieznosci")
+data = open("data/data38.csv")
+csv.reader(data)
+rows = []
+for row in csv.reader(data):
+        rows.append(row)
+data = np.array(rows, dtype=np.float64)
+
+if len(data[0]) != len(data) + 1:
+        print("Nieprawidlowy rozmiar macierzy!")
 else:
-        for i in range(len(res)):
-                print(str(res[i]))
+        A = []
+        b = []
+
+        size = len(data)
+
+        for i in range(size):
+                A.append([])
+                for j in range(size):
+                        A[i].append(data[i][j])
+                b.append(data[i][size])
+
+        res = solve(A, b, None, 0.1)
+        if res == [0]:
+                print("Macierz nie spelnia warunkow zbieznosci")
+        else:
+                for i in range(len(res)):
+                        print(str(res[i]))
